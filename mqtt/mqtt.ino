456ff3f1a399da60ff00aad11418fb94cdf25a0d
julybluetop <neverloveagain2019@outlook.com>
2025-05-08 19:13:34 +0800

    修改了格式，预留了所有传感器数据的上传，并用随机数进行模拟

--- mqtt/mqtt.ino
+++ mqtt/mqtt.ino
@@ -3,22 +3,18 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 #include <ArduinoJson.h>
- 
-#define WLAN_SSID "WIFI6-2.4G"  //wifi名称
-#define WLAN_PASS "200146dd"  //wifi密码
- 
- 
-#define MQTT_SERVER     "broker.emqx.io" //服务器地址，这里使用的是公网所以下面的用户和密码可以随便
- 
-#define MQTT_SERVERPORT 1883
-#define MQTT_USERNAME    "d1"//用户名称和密码，用的公网可以不填可随机
-#define MQTT_KEY         "d1"
- 
-
 #include <Arduino.h>
 #include <Adafruit_Sensor.h>
 #include "DHT.h"
  
+#define WLAN_SSID "WIFI6-2.4G"  //wifi名称
+#define WLAN_PASS "200146dd"  //wifi密码
+ 
+#define MQTT_SERVER     "broker.emqx.io" //服务器地址，这里使用的是公共服务器进行转发
+#define MQTT_SERVERPORT 1883//服务器端口
+#define MQTT_USERNAME    "D1"//用户名称和密码
+#define MQTT_KEY         "D1"
+
 #define DHTTYPE DHT11   // DHT 11
 #define DHTPIN 25
 
@@ -26,17 +22,16 @@ DHT dht(DHTPIN, DHTTYPE);
 
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
-Adafruit_MQTT_Publish pub = Adafruit_MQTT_Publish(&mqtt,"my/mqtt/topic"); // 发布主题
+Adafruit_MQTT_Publish pub = Adafruit_MQTT_Publish(&mqtt,"july/mqtt/topic"); // 发布主题
 
 DynamicJsonDocument doc(128);
-char payload[128];
+char payload[256];
 
- 
 void setup() {
   dht.begin();//启动DHT11
   Serial.begin(115200);
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  delay(2000);
+  delay(1000);
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
   while(WiFi.status() != WL_CONNECTED)
@@ -75,21 +70,25 @@ void MQTT_connect()//连接mqtt服务器，5s连接一次
 }
  
 void loop() {
-  doc["Humidity"] = dht.readHumidity();
+  //doc["Humidity"] = dht.readHumidity();
   doc["Temperature"] = dht.readTemperature();
-  if (isnan(doc["Humidity"]) || isnan(doc["Temperature"])) {
-  Serial.println(F("Failed to read from DHT sensor!"));
+  doc["Oxygen"] = random(900, 1000)/10.0;
+  doc["heartrate"] = random(500, 1300)/10.0;
+  doc["x"] = random(0, 30)/10.0;
+  doc["y"] = random(0, 30)/10.0;
+  doc["z"] = random(0, 30)/10.0;
+  if (isnan(doc["Temperature"] || doc["Oxygen"] || doc["heartrate"] || doc["acceleration"] || doc["x"] || doc["y"] || doc["z"])) {
+  Serial.println(F("Failed to read from sensor!"));
   return;
   }
   MQTT_connect();
-  Serial.print(F("\nSending val "));
   serializeJson(doc, payload);
   if(!pub.publish((uint8_t*)payload, strlen(payload)))//发布数据
   {
     Serial.println(F("Failed"));
   }else
   {
-    Serial.println(F("OK"));
+    Serial.println(payload);
   }
   delay(2000);
 }
